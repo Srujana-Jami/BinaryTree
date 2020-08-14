09f6c8d97951df071f45ce65c85b932f1e981360
Srujana-Jami <srujana.jami@innroad.com>
2020-08-14 18:47:46 +0530

    Add project files.

---
+++ Binary/Demo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Binary
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            Binary bt = new Binary();
+
+            bt.CreateTree();
+
+            bt.Display();
+            Console.WriteLine();
+
+            Console.WriteLine(" Preorder is : ");
+            bt.Preorder();
+
+            Console.WriteLine("   ");
+           
+            
+            Console.WriteLine("Inorder is : ");
+            bt.Inorder();
+
+            Console.WriteLine("   ");
+
+            Console.WriteLine("Postorder is : ");
+            bt.Postorder();
+
+            Console.WriteLine("  ");
+
+            Console.WriteLine("Height of the tree is : " + bt.Height());
+
+        }
+    }
+}
---
+++ Binary/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary
+{
+    class Node
+    {
+
+        public Node lchild;
+        public char info;
+        public Node rchild;
+
+        public Node(char ch)
+        {
+            info = ch;
+            lchild = null;
+            rchild = null;
+
+
+        }
+    }
+}
